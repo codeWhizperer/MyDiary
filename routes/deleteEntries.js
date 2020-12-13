bab930f0902419fa844a1a28fc97e0b92dd5a264
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2020-12-13 10:55:27 -0800

    boostrap diary-api

---
+++ routes/deleteEntries.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const route = express.Router()
+const database = require('../models/post.model')
+
+route.delete('/:id', (req, res) =>{
+    let match = false
+    const {id} = req.params
+for(let i = 0; i < database.Posts.length; i++){
+    if( id == database.Posts[i].id){
+        match = true
+        database.Posts.splice(id, 1)
+        return res.json({status:"success", message:'entry deleted'})
+    }
+}
+if(!match){
+    return res.json('Cannot delete entry')
+}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = route
