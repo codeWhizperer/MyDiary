2a134c2bb21fc70945ab9ef46229b0563cf6b13a
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2020-12-14 01:54:54 -0800

    bootstrap diary-api

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+const database = {
+    users:[
+        {
+            "firstname": "Adegbite",
+            "lastname": "Ademola",
+            "username": "Adegbite.io",
+            "email": "Adegbiteademola1999@gmail.com",
+            "password": "1234"
+        },
+        {
+            "firstname": "Makinde",
+            "lastname": "Oluseun",
+            "username": "oluwaseun",
+            "email": "Max0jay@gmail.com",
+            "password": "0409199"
+        },
+        {
+            "firstname": "Makinde",
+            "lastname": "Oluwasegun",
+            "username": "segxy",
+            "email": "segun@gmail.com",
+            "password": "love"
+        }
+
+    ]
+}
+
+
+module.exports = database
+
+
