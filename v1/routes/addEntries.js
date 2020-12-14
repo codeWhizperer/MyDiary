d5ab6a1c04ee7e934abf933d0c83abb50c564e75
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2020-12-14 00:14:49 -0800

    bootstrap diary-api

---
+++ v1/routes/addEntries.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const route = express.Router()
+const database = require('../models/post.model')
+
+route.post('/addEntries', (req,res)=>{
+    const {title, description} = req.body
+    const databaseLength = database.Posts.length
+    let oldId = database.Posts[databaseLength - 1].id
+    let newId = oldId 
+    database.Posts.push({
+        id:newId + 1,
+        title:title,
+        description:description,
+        createdOn: Date()
+    })
+res.json({
+    status:"success",
+    data: {
+        id:oldId + 1,
+        message:"entry successfully created",
+        createdOn:Date(),
+        title:title,
+        description:description
+    }
+})
+})
+
+
+
+
+
+
+
+
+
+
+module.exports = route
---
+++ v1/routes/editEntries.js
@@ -0,0 +1,46 @@
+const express =  require('express')
+const route = express.Router();
+const database = require('../models/post.model')
+
+
+route.patch('/:id', (req,res)=>{
+const {id} = req.params
+const {title, description} = req.body
+let match = false
+for(let i = 0; i < database.Posts.length; i++){
+if(id == database.Posts[i].id){
+match = true
+let updatedEntry = database.Posts[i].id
+ updatedEntry = ({id:id, title:title, description:description, createdOn:Date()})
+ database.Posts.splice(id , 1, updatedEntry)
+return res.json({status:"Success entry successfully updated!", data:updatedEntry})
+}
+}
+if(!match){
+  return res.json('Cannot find entry')
+}
+
+})
+
+
+// select id of array and equate it to data coming from frontend
+
+// route.patch('/:id', (req,res)=>{
+//   const {id} = req.params
+//   const {title, description} = req.body
+//   let match = false
+//   for(let i = 0; i < database.Posts.length; i++){
+//   if(database.Posts[i].id === id){
+//   database.Posts.push({
+//   id:id,
+//   title:title,
+//   description:description,
+//   createdOn:Date()
+//   })
+//   return res.json({status:"success", message:"entry updated successfully",data:database.Posts})
+//   }else{
+//     res.json('err')
+//   }
+//   }
+
+module.exports = route
