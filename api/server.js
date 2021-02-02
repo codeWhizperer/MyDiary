952bc4f818f102e6b02eb91d3fae0096411e324b
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2021-02-02 13:10:36 -0800

    integrated database

--- api/server.js
+++ api/server.js
@@ -1,34 +1,34 @@
-const { request } = require('express');
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express();
-require('dotenv').config()
+ const { request } = require('express');
+ const express = require('express')
+ var bodyParser = require('body-parser')
+ const app = express();
+ require('dotenv').config()
 
-const port =5000
-//Middleware
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-app.use(express.json())
-const path = require('./routes/index')
-
-
-app.get('/', (req, res)=>{
-    res.status(200).json('Welcome to Homepage')
-})
-
-// middleware auth route
-app.use('/api/v1/', path)
-app.get('*', (req,res)=>{
-    res.send('Page doesn"/t exist' )
-})
+ const port =5000
+ //Middleware
+ app.use(bodyParser.json())
+ app.use(
+   bodyParser.urlencoded({
+     extended: true,
+   })
+ )
+ app.use(express.json())
+ const path = require('./routes/index')
 
 
+ app.get('/', (req, res)=>{
+     res.status(200).json('Welcome to Homepage')
+ })
 
-app.listen(port , (req, res)=>{
-    console.log(`Server is runnung on ${port}`)
-})
-module.exports = app;
+ // middleware auth route
+ app.use('/api/v1/', path)
+ app.get('*', (req,res)=>{
+     res.send('Page doesn"/t exist' )
+ })
+
+
+
+ app.listen(port , (req, res)=>{
+     console.log(`Server is runnung on ${port}`)
+ })
+ module.exports = app;
